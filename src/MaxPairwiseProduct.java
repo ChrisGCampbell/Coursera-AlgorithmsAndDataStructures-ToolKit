77bc6e1b32ebbbaabad50274c6b454aa9ddd1f98
Chris Campbell <ccampbll3@gatech.edu>
2019-04-26 18:06:37 -0400

    optimized algorithm

--- src/MaxPairwiseProduct.java
+++ src/MaxPairwiseProduct.java
@@ -19,7 +19,7 @@ public class MaxPairwiseProduct {
             index2 = 1;
         }
         else {
-             index2 = 0;
+            index2 = 0;
         }
 
         for(int i = 0; i < n; i++){
@@ -30,6 +30,66 @@ public class MaxPairwiseProduct {
 
         return numbers[index1] * numbers[index2];
 
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        long[] numbers = new long[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.println(getMaxPairwiseProduct(numbers));
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new
+                        InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+}
+
+// ********************************************* ////////////////////////
+import java.util.*;
+        import java.io.*;
+        import java.util.Arrays;
+
+        // O(n log n)
+
+public class MaxPairwiseProduct {
+    static long getMaxPairwiseProduct(long[] numbers) {
+
+        long max_product=0;
+        int n = numbers.length;
+        Arrays.sort(numbers);
+        max_product = numbers[n - 1] * numbers[n - 2];
+
+
+        return max_product;
     }
 
     public static void main(String[] args) {
